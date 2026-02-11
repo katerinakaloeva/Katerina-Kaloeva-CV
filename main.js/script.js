c37ea35d0aedf35128ccc9e0408d404e0963ea3d
katerinakaloeva <kamila-kaloeva@hotmail.com>
2026-02-12 00:04:39 +0200

    mycomm

---
+++ main.js/script.js
@@ -0,0 +1,206 @@
+// ====== TRANSLATIONS ======
+const translations = {
+  el: {
+    nav_about: "About Me",
+    nav_education: "Education",
+    nav_experience: "Work Experience",
+    nav_portfolio: "Portfolio",
+
+    hello: "Hi, I am",
+    name: "Κατερίνα Καλόεβα",
+    location: "📍 Άλιμος, Αθήνα",
+
+    // about
+    about_title: "Σχετικά με εμένα",
+    about_p1: "Κάτοχος δύο πτυχίων στους τομείς της Αισθητικής και της Διοίκησης Τουριστικών Επιχειρήσεων. Διαθέτω συνέπεια και καλή ικανότητα προσαρμογής σε διαφορετικά εργασιακά περιβάλλοντα, καθώς και αποτελεσματική συνεργασία και επικοινωνία σε ομαδικό πλαίσιο.",
+    about_p2: "Πλέον έχω στραφεί στον τομέα της τεχνολογίας, με έμφαση στον προγραμματισμό και τον σχεδιασμό ιστοσελίδων, διαθέτοντας βασικές γνώσεις HTML,CSS και JavaScript. Αυτή την περίοδο ολοκληρώνω πιστοποίηση Junior Graphic Designer μέσω προγράμματος της ΔΥΠΑ, ενισχύοντας τις δημιουργικές και τεχνικές μου δεξιότητες. Στόχος μου είναι η συνεχής μάθηση και η σταδιακή εξέλιξη στον ψηφιακό τομέα.",
+    
+    skills_title: "Δυνατά Σημεία",
+    skill_1: "Πρακτικότητα & Επίλυση Προβλημάτων",
+    skill_2: "Δημιουργία σχέσεων Εμπιστοσύνης",
+    skill_3: "Συνέπεια και ομαδικό πνεύμα συνεργασίας",
+    skill_4: "Ευγένεια & Υπομονή",
+
+    languages_title: "Γλώσσες",
+    lang_gr: "Ελληνικά",
+    lang_ru: "Ρωσικά",
+    lang_en: "Αγγλικά",
+
+    // TRAINING & EDUCATION
+    education_title: "Εκπαίδευση & Κατάρτιση",
+    certifications_title: "Πιστοποιήσεις & Κατάρτιση",
+    education_group_title: "Εκπαίδευση",
+
+    edu1_date: "12/2026 – σήμερα",
+    edu1_title: "React JS Developer for Entry-Level Job (e-learning)",
+    edu1_place: "ΕΚΠΑ – Κέντρο e-Learning",
+
+    edu2_date: "2025 – σήμερα",
+    edu2_title: "Πιστοποίηση Junior Graphic Designer",
+    edu2_place: "ΔΥΠΑ",
+
+    edu3_date: "11/2025 – 01/2026",
+    edu3_title: "JavaScript – Web Development Masterclass (React & Node.js)",
+    edu3_place: "Udemy (Online) — 130 ώρες",
+
+    edu4_date: "09/2015 – 02/2020",
+    edu4_title: "Πτυχίο Διοίκησης Τουριστικών Επιχειρήσεων",
+    edu4_place: "Πανεπιστήμιο СОГУ (North Ossetian State University), Ρωσία",
+
+    edu5_date: "10/2015 – 06/2017",
+    edu5_title: "Τεχνικός Αισθητικής Τέχνης και Μακιγιάζ",
+    edu5_place: "ΙΕΚ ΑΚΜΗ, Αθήνα",
+
+    // WORK
+    experience_title: "Επαγγελματική Εμπειρία",
+    exp1_date: "Aesthetic Athens – Plastic Surgery Clinic",
+    exp1_role: "Αισθητικός",
+    exp1_task1: "Θεραπείες προσώπου & σώματος",
+
+    exp2_date: "01/2021 – 09/2021",
+    exp2_role: "Σύμβουλος Προϊόντων / Άμεσες Πωλήσεις",
+    exp2_task1: "Προώθηση προϊόντων",
+
+    exp3_date: "02/2018 – 12/2019",
+    exp3_role: "Αισθητικός",
+    exp3_task1: "Πρακτική άσκηση διάρκειας 6 μηνών με μετέπειτα μόνιμη συνεργασία",
+    exp3_task2: "Θεραπείες προσώπου & σώματος",
+    exp3_task3: "Υποστήριξη reception (εξυπηρέτηση πελατών, ραντεβού, ταμείο)",
+
+    // PORTFOLIO
+    portfolio_title: "Portfolio",
+    project1_title: "Personal CV",
+    project1_desc: "Σχεδιασμός και ανάπτυξη δυναμικού βιογραφικού με δυνατότητα μετάφρασης.",
+    project2_title: "Website for Internet Cafe",
+    project2_desc: "Σχεδιασμός σύγχρονης ιστοσελίδας για την προβολή των υπηρεσιών ενός Internet Cafe.",
+    view_project: "Δείτε το Project"
+  },
+
+  en: {
+    nav_about: "About Me",
+    nav_education: "Education",
+    nav_experience: "Work Experience",
+    nav_portfolio: "Portfolio",
+
+    hello: "Hi, I am",
+    name: "Katerina Kaloeva",
+    location: "📍 Alimos, Athens",
+
+    about_title: "About Me",
+    about_p1: "Holder of two degrees in Aesthetics and Tourism Business Administration, with strong adaptability to diverse work environments and effective collaboration and communication skills within team settings.",
+    about_p2: "Now i have shifted my focus to the field of technology, with an emphasis on programming and web design, possessing basic knowledge of HTML,CSS and JavaScript. At this time, I am completing a Junior Graphic Designer certification through a DYPA program, further strengthening my creative and technical skills. My goal is continuous learning and gradual development in the digital sector.",
+    
+    skills_title: "Key Skills",
+    skill_1: "Practicality & Problem Solving",
+    skill_2: "Building Trusting Relationships",
+    skill_3: "Consistency and Teamwork Spirit",
+    skill_4: "Courtesy & Patience",
+
+    languages_title: "Languages",
+    lang_gr: "Greek",
+    lang_ru: "Russian",
+    lang_en: "English",
+
+    portfolio_title: "Portfolio",
+    education_title: "Education & Training",
+    certifications_title: "Certifications & Training",
+    education_group_title: "Education",
+
+    edu1_date: "12/2026 – Present",
+    edu1_title: "React JS Developer for Entry-Level Job (e-learning)",
+    edu1_place: "National and Kapodistrian University of Athens – e-Learning",
+
+    edu2_date: "2025 – Present",
+    edu2_title: "Junior Graphic Designer Certification",
+    edu2_place: "DYPA",
+
+    edu3_date: "11/2025 – 01/2026",
+    edu3_title: "JavaScript – Web Development Masterclass (React & Node.js)",
+    edu3_place: "Udemy (Online) — 130 hours",
+
+    edu4_date: "09/2015 – 02/2020",
+    edu4_title: "Bachelor’s Degree in Tourism Business Administration",
+    edu4_place: "North Ossetian State University (SOGU), Russia",
+
+    edu5_date: "10/2015 – 06/2017",
+    edu5_title: "Aesthetics & Makeup Technician",
+    edu5_place: "IEK AKMI, Athens",
+
+    experience_title: "Work Experience",
+    exp1_date: "Aesthetic Athens – Plastic Surgery Clinic",
+    exp1_role: "Aesthetician",
+    exp1_task1: "Facial and body treatments",
+
+    exp2_date: "01/2021 – 09/2021",
+    exp2_role: "Product Advisor / Direct Seller",
+    exp2_task1: "Product promotion",
+
+    exp3_date: "02/2018 – 12/2019",
+    exp3_role: "Aesthetician",
+    exp3_task1: "Six-month internship followed by permanent collaboration",
+    exp3_task2: "Facial and body treatments",
+    exp3_task3: "Reception support (customer service, appointments, cashier)",
+
+    project1_title: "Personal CV",
+    project1_desc: "Design and development of a dynamic CV with translation capabilities.",
+    project2_title: "Website for Internet Cafe",
+    project2_desc: "Modern website design for showcasing the services of an Internet Cafe.",
+    view_project: "View Project"
+  }
+};
+const dataKey = document.querySelectorAll("[data-key]")
+
+function setLanguage(lang) {
+  dataKey.forEach(element => {
+    const key = element.dataset.key
+    if (translations[lang][key]) {
+      element.textContent = translations[lang][key];
+    }
+
+
+  });
+}
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const langButtons = document.querySelectorAll(".lang")
+
+  const savedLang = localStorage.getItem("language") || "el";
+  setLanguage(savedLang);
+
+  langButtons.forEach(ele => {
+
+    const btnLang = ele.textContent.toLowerCase()
+    ele.classList.remove("active")
+    if (btnLang === savedLang) {
+      ele.classList.add("active")
+    }
+  })
+
+
+
+
+
+
+  langButtons.forEach(item => {
+    item.addEventListener("click", () => {
+      const langs = item.textContent.toLowerCase()
+      setLanguage(langs)
+      localStorage.setItem("language", langs)
+
+
+      langButtons.forEach(btn => {
+        btn.classList.remove("active")
+      })
+
+
+
+
+      item.classList.add("active")
+
+
+    })
+
+  })
+})
